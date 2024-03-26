c33e59adfabef572756b94dd322c344de4edc153
Daniel <danielbeloded@gmail.com>
2024-03-26 13:42:00 +0200

    added responsive design

--- src/components/crew/Crew.js
+++ src/components/crew/Crew.js
@@ -1,62 +1,57 @@
-import {React, useState} from 'react'
-import './crew.scss';
-import Nav from '../nav/Nav';
-import data from '../../data.json'
+import { React, useState } from "react";
+import "./crew.scss";
+import Nav from "../nav/Nav";
+import data from "../../data.json";
 
-import commander from '../../assets/crew/image-douglas-hurley.png'
-import mark from '../../assets/crew/image-mark-shuttleworth.png'
+import commander from "../../assets/crew/image-douglas-hurley.png";
+import mark from "../../assets/crew/image-mark-shuttleworth.png";
 
-import victor from '../../assets/crew/image-victor-glover.png'
+import victor from "../../assets/crew/image-victor-glover.png";
 
-import ansari from '../../assets/crew/image-anousheh-ansari.png'
+import ansari from "../../assets/crew/image-anousheh-ansari.png";
 const Crew = () => {
-   
-  const [crew] = useState(data.crew)
-  const [value, setValue] = useState(0)
+  const [crew] = useState(data.crew);
+  const [value, setValue] = useState(0);
 
-  const {name, role, bio} = crew[value]
+  const { name, role, bio } = crew[value];
 
-  const imgArr = [commander, mark, victor, ansari]
+  const imgArr = [commander, mark, victor, ansari];
 
   return (
-    <div className='crew'>
-        <Nav/>
-        <div className="crew__wrapper">
+    <div className="crew">
+      <Nav />
+      <div className="crew__wrapper">
         <div className="crew__descr-wrapper">
-        <div className="crew__title">
-        <span>02</span>
-        Meet your crew
-        </div>
+          <div className="crew__title">
+            <span>02</span>
+            Meet your crew
+          </div>
 
-        <div className="crew__job">
-        {role} 
-        </div>
-        <div className="crew__name">
-        {name}
-        </div>
+          <div className="crew__job">{role}</div>
+          <div className="crew__name">{name}</div>
 
-        <div className="crew__descr">
-        {bio}
-        </div>
+          <div className="crew__descr">{bio}</div>
 
-        <div className="crew__btn">
-        <div className='crew__btn-list'>
-            {crew.map((item, index) => (
-              <button key={index} className={`crew__btn-item ${index === value && "active__crew "}`} onClick={() => setValue(index)}></button>
-            ))}
+          <div className="crew__btn">
+            <div className="crew__btn-list">
+              {crew.map((item, index) => (
+                <button
+                  key={index}
+                  className={`crew__btn-item ${
+                    index === value && "active__crew "
+                  }`}
+                  onClick={() => setValue(index)}
+                ></button>
+              ))}
+            </div>
+          </div>
         </div>
-        </div>
-
-        
-
-        </div>
-
-        <div className="crew__img">
-            <img src={imgArr[value]} alt={name} />
-        </div>
-        </div>
+      </div>
+      <div className="crew__img">
+        <img src={imgArr[value]} alt={name} />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Crew
+export default Crew;
--- src/components/tech/Tech.js
+++ src/components/tech/Tech.js
@@ -1,66 +1,59 @@
-import {React, useState}from 'react'
-import './tech.scss'
-import Nav from '../nav/Nav'
-import launch from '../../assets/technology/image-launch-vehicle-portrait.jpg'
-import spaceport from '../../assets/technology/image-spaceport-portrait.jpg'
-import capsule from '../../assets/technology/image-space-capsule-portrait.jpg'
-import data from '../../data.json'
+import { React, useState } from "react";
+import "./tech.scss";
+import Nav from "../nav/Nav";
+import launch from "../../assets/technology/image-launch-vehicle-portrait.jpg";
+import spaceport from "../../assets/technology/image-spaceport-portrait.jpg";
+import capsule from "../../assets/technology/image-space-capsule-portrait.jpg";
+import data from "../../data.json";
+import launchLand from "../../assets/technology/image-launch-vehicle-landscape.jpg";
+import spaceportLand from "../../assets/technology/image-spaceport-landscape.jpg";
+import capsuleLand from "../../assets/technology/image-space-capsule-landscape.jpg";
 const Tech = () => {
+  const [tech] = useState(data.technology);
+  const [value, setValue] = useState(1);
 
-  const [tech] = useState(data.technology)
-  const [value, setValue] = useState(0)
+  const { name, description } = tech[value];
 
-  const {name, description} = tech[value]
-
-  const imgArr = [launch, spaceport, capsule]
-
-  
+  const imgArr = [launch, spaceport, capsule];
+  const imgLand = [launchLand, spaceportLand, capsuleLand];
 
   return (
-
-    <div className='tech'>
-    <Nav></Nav>    
-    <div className="tech__title">
+    <div className="tech">
+      <Nav></Nav>
+      <div className="tech__title">
         <span>02</span>
         SPACE LAUNCH 101
-        </div>
-        <div className="tech__wrapper">
+      </div>
 
-       
+      <img className="tech__land" src={imgLand[value]} alt="" />
+      <div className="tech__wrapper">
         <div className="tech__info-wrapper">
-           <div className="tech__descr-wrapper">
-           <div className="tech__btn-list">
-                {tech.map((item, index) => (
-                  <button key={index} 
-                  className={`tech__btn-item ${index === value && "active-tech"}`}
-                  onClick={() => setValue(index)}><span>{index + 1}</span></button>
-                ))}
-            </div>
-
-           
-                  
-          <div className="wrapper__tech">
-          <div className="tech__name">
-            THE TERMINOLOGY…
-            </div>
-
-            <div className="tech__subtitle">
-            {name}
-            </div>
-
-            <div className="tech__descr">
-            {description}
-            </div>
+          <div className="tech__btn-list">
+            {tech.map((item, index) => (
+              <button
+                key={index}
+                className={`tech__btn-item ${index === value && "active-tech"}`}
+                onClick={() => setValue(index)}
+              >
+                <span>{index + 1}</span>
+              </button>
+            ))}
           </div>
-           </div>
-            
-        </div>
-        <div className="tech__img">
-                <img src={imgArr[value]} alt={name} />
-            </div>
-        </div>
-    </div>
-  )
-}
+          <div className="wrapper__tech">
+            <div className="tech__name">THE TERMINOLOGY…</div>
 
-export default Tech
+            <div className="tech__subtitle">{name}</div>
+
+            <div className="tech__descr">{description}</div>
+          </div>
+        </div>
+
+        <div className="tech__img">
+          <img className="tech__img" src={imgArr[value]} alt={name} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Tech;
